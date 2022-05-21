2430acf2c573f1027ff6d05db6910c3eed263338
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-05-21 19:59:49 +0530

    js-lic

---
+++ js/AlphaTheme.js
@@ -0,0 +1,5 @@
+/*!
+ * Alpha Theme v1.0.0 (https://github.com/Mindula-Dilthushan/Alpha-Theme/)
+ * Copyright 2022 The Alpha Theme Author (https://github.com/Mindula-Dilthushan)
+ * Licensed under MIT (https://github.com/Mindula-Dilthushan/Alpha-Theme/blob/master/LICENSE)
+ */
